7731c7aba39eee015d48b58fae6807c30e1eae5f
ASif <asif0531@live.com>
2014-01-08 09:30:28 +0600

    start writing models

---
+++ ePOS.UI/ePOS.DataAccessLayer/BussinessObject/Customer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ePOS.DataAccessLayer.BussinessObject
+{
+   public class Customer
+    {
+       private int _CustomerCode;
+       private string _CustomerName;
+       private string _CustomerAddress;
+       private string _CustomerEmail;
+       private string _CustomerPhoneNumber;
+       private string _CustomerWeb;
+       private string _CustomerAccountNumber;
+       private bool _CustomerTaxable;
+       private string _CustomerRemark;
+       private string _CustomerCity;
+       private int _CustomerZip;
+       
+
+       public Customer()
+       {
+           _CustomerCode = 0; 
+           _CustomerName = string.Empty;
+           _CustomerAddress = string.Empty;
+           _CustomerEmail = string.Empty;
+           _CustomerPhoneNumber = string.Empty;
+           _CustomerWeb = string.Empty;
+           _CustomerAccountNumber = string.Empty;
+           _CustomerTaxable = false;
+           _CustomerZip = 0;
+           _CustomerCity = string.Empty;
+           _CustomerRemark = string.Empty;
+
+       }
+
+      
+        public int CustomerCode
+        {
+            get { return _CustomerCode; }
+            set { _CustomerCode = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return _CustomerName; }
+            set { _CustomerName = value; }
+        }
+
+        public string CustomerAddress
+        {
+            get { return _CustomerAddress; }
+            set { _CustomerAddress = value; }
+        }
+
+        public string CustomerEmail
+        {
+            get { return _CustomerEmail; }
+            set { _CustomerEmail = value; }
+        }
+
+       
+        public string CustomerPhoneNumber
+        {
+            get { return _CustomerPhoneNumber; }
+            set { _CustomerPhoneNumber = value; }
+        }
+
+        public string CustomerWeb 
+        {
+            get { return _CustomerWeb; }
+            set { _CustomerWeb = value; }
+        }
+
+        public string AccountNumber
+        {
+            get { return _CustomerAccountNumber; }
+            set { _CustomerAccountNumber = value; }
+        }
+
+        public bool CustomerTaxable
+        {
+            get { return _CustomerTaxable; }
+            set { _CustomerTaxable = value; }
+        }
+
+        public string CustomerRemark
+        {
+            get { return _CustomerRemark; }
+            set { _CustomerRemark = value; }
+        }
+
+
+        public string CustomerCity
+        {
+            get { return _CustomerCity; }
+            set { _CustomerCity = value; }
+        }
+
+
+        public int CustomerZip
+        {
+            get { return _CustomerZip; }
+            set { _CustomerZip = value; }
+        }
+        
+        
+        
+        
+
+    }
+
+
+}
---
+++ ePOS.UI/ePOS.DataAccessLayer/BussinessObject/Supplier.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ePOS.DataAccessLayer.BussinessObject
+{
+   public class Supplier
+    {
+
+       private string _CompanyName;
+       private string _SupplierFirstName;
+       private string _SupplierLastName;
+       private string _SupplierEmail;
+       private string _SupplierPhoneNumber;
+       private string _SupplierAddress;
+       private string _SupplierCity;
+       private int _SupplierZip;
+       private string _SupplierAccountNumber;
+       private string _SupplierRemark;
+
+       public Supplier()
+       {
+           _CompanyName = string.Empty;
+           _SupplierFirstName = string.Empty;
+           _SupplierLastName = string.Empty;
+           _SupplierEmail = string.Empty;
+           _SupplierPhoneNumber = string.Empty;
+           _SupplierAddress = string.Empty;
+           _SupplierCity = string.Empty;
+           _SupplierZip = 0;
+           _SupplierAccountNumber = string.Empty;
+           _SupplierRemark = string.Empty;
+       }
+
+
+       public string CompanyName
+       {
+           get { return _CompanyName; }
+           set { _CompanyName = value; }
+       }
+
+       public string SupplierFirstname
+       {
+           get { return _SupplierFirstName; }
+           set { _SupplierFirstName = value; }
+       }
+
+       public string SupplierLastName
+       {
+           get { return _SupplierLastName; }
+           set { _SupplierLastName = value; }
+       }
+
+
+       public string SupplierEmail
+       {
+           get { return _SupplierEmail; }
+           set { _SupplierEmail = value; }
+       }  
+
+       public string SupplierPhoneNumber
+       {
+           get { return _SupplierPhoneNumber; }
+           set { _SupplierPhoneNumber = value; }
+       }
+
+       public string Supplieraddress
+       {
+           get { return _SupplierAddress; }
+           set { _SupplierAddress = value; }
+       }
+
+      
+       public string SupplierCity
+       {
+           get { return _SupplierCity; }
+           set { _SupplierCity = value; }
+       }
+       
+       public int Supplierip
+       {
+           get { return _SupplierZip; }
+           set { _SupplierZip = value; }
+       }
+
+      
+       public string SupplierAccountNumber
+       {
+           get { return _SupplierAccountNumber; }
+           set { _SupplierAccountNumber = value; }
+       }
+
+      
+       public string SupplierRemark
+       {
+           get { return _SupplierRemark; }
+           set { _SupplierRemark = value; }
+       }
+        
+        
+        
+         
+        
+
+        
+
+    }
+}
